c52af15ae1c48792d126486ef95ec39b596d884d
xiaodong <565071984@qq.com>
2018-08-14 18:02:49 +0800

    同步代码

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
+
+//忽略isMounted废弃警告提示，不影响程序运行
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 AppRegistry.registerComponent('AwesomeProject', () => App);
--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  WebView,
+  Dimensions,
+  TouchableHighlight,
+} from 'react-native';
+// import  speedTest  from 'speedtest-net';
+// var test = speedTest({maxTime: 5000});
 
+const {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,15 +23,62 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  webWrap: {
+    width: width,
+    height: height,
+  }
 });
+class ProfileScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.data = 0;
+    
+  }
+  componentDidMount(){
+    // speedTest.visual({maxTime: 5000}, (err, data) => {
+    //   console.dir(data);
+    // });
+  }
+  // handleMessage = (event) => {
+  //   //webview中的html页面传过来的的数据在event.nativeEvent.data上
+  //   console.log(event.nativeEvent.data);
+  // }
+  handleMessage = (evt) => {
+      const message = JSON.parse(evt.nativeEvent.data)
+      if (message.command === 'get info') {
+          const nickname = message.payload.nickname
+          // 得到了数据随你怎么办吧
+          console.log(nickname, 'RN获取数据');
+      }
+  }
 
-const ProfileScreen = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Profile Screen
-    </Text>
-  </View>
-);
+  //向html页面发送消息
+  sendMessage() {
+    // this.webview && this.webview.postMessage(++this.data);
+    const data = {
+      command: 'get info', // 表明意图
+      payload: { // 表明内容
+          property: 'nickname'
+      }
+  }
+  this.webview.postMessage(JSON.stringify(data))
+  }
+  render() {
+    let url = '../common/PostMessage.html';
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Profile Screen
+        </Text>
+        <TouchableHighlight onPress={() => {
+                    this.sendMessage()
+                }}><Text>发送数据到WebView</Text></TouchableHighlight>
+        <WebView onMessage={this.handleMessage} source={require(url)} style={styles.webWrap} ref={webview => this.webview = webview}/>
+        
+      </View>
+    );
+  }
+}
 
 ProfileScreen.navigationOptions = {
   title: 'Profile',
