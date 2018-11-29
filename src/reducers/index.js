2be2bc1539b114c2787da196b8dc6889a732b985
xiaodong <565071984@qq.com>
2018-11-29 16:57:28 +0800

    文件更新

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,13 @@
-import nav from './navReducer';  //navigation
-//import nav from './navTabReducer';  //tabNavigation
+// import nav from './navReducer';  //navigation
+import nav from "./navTabReducer"; //tabNavigation
 //import nav from './navDrawerReducer';  //Drawernavigation
-import auth from './authReducer';
+import auth from "./authReducer";
 
-import { combineReducers } from 'redux';
-
+import { combineReducers } from "redux";
 
 const AppReducer = combineReducers({
   nav,
-  auth,
+  auth
 });
 
 export default AppReducer;
